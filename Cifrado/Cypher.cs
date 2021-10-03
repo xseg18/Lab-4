467402051d5160156f4305ad9ba9fea52f746dba
Ximena Elizardi <ximenagobern@gmail.com>
2021-10-02 19:03:39 -0600

    Libreria agregada

---
+++ Cifrado/Cypher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Cifrado
+{
+   public interface CYPHER
+    {
+        string Cifrar(string original);
+        string Descifrar(string cifrado);
+    }
+
+    public class Cesar: CYPHER
+    {
+
+        public string Cifrar(string cifrar)
+        {
+            string cifrado = "";
+
+            return cifrado;
+        }
+        public string Descifrar(string cifrado)
+        {
+            string descifrado = "";
+
+            return descifrado;
+        }
+    }
+
+    public class ZigZag: CYPHER
+    {
+        public string Cifrar(string cifrar)
+        {
+            string cifrado = "";
+
+            return cifrado;
+        }
+        public string Descifrar(string cifrado)
+        {
+            string descifrado = "";
+
+            return descifrado;
+        }
+    }
+}
