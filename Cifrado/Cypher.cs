8e1a6d7fdba192db38eb3af3fbb7d2e4177f6845
Ximena Elizardi <ximenagobern@gmail.com>
2021-10-02 20:26:41 -0600

    cifrado pt 1

--- Cifrado/Cypher.cs
+++ Cifrado/Cypher.cs
@@ -14,11 +14,34 @@ namespace Cifrado
 
     public class Cesar: CYPHER
     {
-
+        string clave = "%12([centrifugados CENTRIFUGADOS])34$";
+        Dictionary<char, int> cipher = new Dictionary<char, int>();
+        Dictionary<int, char> abc = new Dictionary<int, char>();
         public string Cifrar(string cifrar)
         {
+            //diccionario normal
+            for (int i = 0; i < 256; i++)
+            {
+                abc.Add(i, Convert.ToChar(i));
+            }
+            //nuevo abecedario
+            for (int i = 0; i < clave.Length; i++)
+            {
+                cipher.Add(clave[i], i);
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                if (!cipher.ContainsKey(abc[i]))
+                {
+                    cipher.Add(abc[i], cipher.Count);
+                }
+            }
+            //cifrado
             string cifrado = "";
-
+            foreach(char l in cifrar)
+            {
+                cifrado += cipher[abc[Convert.ToInt32(l)]];
+            }
             return cifrado;
         }
         public string Descifrar(string cifrado)
